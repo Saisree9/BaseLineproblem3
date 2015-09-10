dc6592bc44b075985c714675586dba9e82e2d621
saisree <saisree@INsaisree.local>
2015-09-10 11:51:36 +0530

    added spec for display method and implemented

--- src/com/tw/baselineproblem/Console.java
+++ src/com/tw/baselineproblem/Console.java
@@ -6,6 +6,9 @@ public class Console {
     public String getInput() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
+    }
 
+    public void display(String receipt){
+        System.out.println(receipt);
     }
 }
--- test/com/tw/baselineproblem/ConsoleTest.java
+++ test/com/tw/baselineproblem/ConsoleTest.java
@@ -1,17 +1,32 @@
 package com.tw.baselineproblem;
 
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
 public class ConsoleTest {
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+
     @Test
     public void shouldGetInputFromCustomer() {
         Console input = mock(Console.class);
-        when(input.getInput()).thenReturn("a\n b");
-        assertEquals("a\n b", input.getInput());
+        when(input.getInput()).thenReturn("1 book at 12.49");
+        assertEquals("1 book at 12.49", input.getInput());
+    }
+
+    @Test
+    public void shouldDisplayReceipt() {
+        System.setOut(new PrintStream(output));
+        Console displayReceipt = mock(Console.class);
+        displayReceipt.display("1 book at 12.49");
+        Mockito.verify(displayReceipt, times(1)).display("1 book at 12.49");
     }
 
 }
