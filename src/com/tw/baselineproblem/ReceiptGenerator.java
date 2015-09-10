973d897adef6399abc28616365112d431d849423
saisree <saisree@INsaisree.local>
2015-09-10 12:26:25 +0530

    added spec for receipt generator which prints receipts

---
+++ src/com/tw/baselineproblem/ReceiptGenerator.java
@@ -0,0 +1,26 @@
+package com.tw.baselineproblem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReceiptGenerator {
+    private List<String[]> listOfSplitedItems = new ArrayList<>();
+    private Double totalCost = 0.0;
+
+    public void printReceipt(String exemptedItems, Console console) {
+        calculateTotalCost(exemptedItems);
+        console.display(exemptedItems + "SalesTax:0.00" + "totalCost:" + totalCost);
+
+    }
+
+    public void calculateTotalCost(String exemptedItems) {
+        String[] listOfItems = exemptedItems.split("\n");
+        for (String str : listOfItems) {
+            listOfSplitedItems.add(str.split(" "));
+        }
+        for (String[] strings : listOfSplitedItems) {
+            totalCost += new Double(strings[strings.length - 1]);
+        }
+    }
+
+}
---
+++ test/com/tw/baselineproblem/ReceiptGeneratorTest.java
@@ -0,0 +1,17 @@
+package com.tw.baselineproblem;
+
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class ReceiptGeneratorTest {
+    @Test
+    public void shouldGenerateReceiptForExemptedItem() {
+        ReceiptGenerator receiptGenerator = new ReceiptGenerator();
+        String exemptedItems = "1 book at 12,49\n";
+        Console console = mock(Console.class);
+        receiptGenerator.printReceipt(exemptedItems, console);
+        verify(console).display("1 book at 12,49\nSalesTax:0.00\ntotalCost:12.49");
+    }
+}
